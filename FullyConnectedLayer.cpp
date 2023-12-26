aa31d5249b0f0bdd4f21962252e18bd997bf2c0b
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2023-12-26 07:28:22 -0500

    create layer

---
+++ FullyConnectedLayer.cpp
@@ -0,0 +1,10 @@
+#import <iostream>
+#import <vector>
+#import <cmath>
+
+class FullyConnectedLayer {
+public:
+    FullyConnectedLayer (int noNodes): noNodes(noNodes) {
+        
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,4 @@
+// main function
+// load training data here
+// initialize a network object
+// output into a csv file
