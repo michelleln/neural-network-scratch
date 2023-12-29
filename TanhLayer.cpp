a5679777a85ad79166c8d075a8f8bdbb20da4cd3
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2023-12-29 09:40:05 -0500

    Used Tanh instead of Sigmoid for activation

--- TanhLayer.cpp
+++ TanhLayer.cpp
@@ -1,14 +1,14 @@
 #include <cmath>
 #include "Matrix.cpp"
 
-class SigmoidLayer{
+class TanhLayer{
 public:
     static Matrix<float> forwardPropagate(const Matrix<float> &output) {
         return Matrix<float>::opElement(output, sigmoid);
     }
 
     static Matrix<float> getDerivatives(const Matrix<float> &thisLayerOutput, const Matrix<float> &nextLayerDerivatives) {
-        Matrix<float> layerInputDerivatives = Matrix<float>::opElement(previousLayerOutput, [](float n) {return std::exp(-n) / std::pow(1.0f + std::exp(-n), 2.0f));
+        Matrix<float> layerInputDerivatives = Matrix<float>::opElement(previousLayerOutput, [](float n) {return 1 - std::pow(n, 2.0f)); // derivative of tanhx is 1 - tanhx squared
         layerInputDerivatives.hProduct(nextLayerDerivatives);
 
         return layerInputDerivatives;
