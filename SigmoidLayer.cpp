72f78c9840a8f3479f08e4b5ebf7664e5ed17daf
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2023-12-29 03:48:54 -0500

    Update SigmoidLayer.cpp

--- SigmoidLayer.cpp
+++ SigmoidLayer.cpp
@@ -3,5 +3,11 @@
 
 class SigmoidLayer{
 public:
-    Matrix<float> 
+    static Matrix<float> forwardPropagate(const Matrix<float> &output) {
+        return Matrix<float>::opElement(output, sigmoid);
+    }
+
+    static Matrix<float> getDerivatives(const Matrix<float> &layerInputDerivatives, const Matrix<float> &nextLayerDerivatives) {
+        
+    }
 }
