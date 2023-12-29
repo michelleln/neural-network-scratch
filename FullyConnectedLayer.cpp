97d4a4d8a0c4931bbc3edc645ba37cd724390488
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2023-12-29 10:16:43 -0500

    Update FullyConnectedLayer.cpp

--- FullyConnectedLayer.cpp
+++ FullyConnectedLayer.cpp
@@ -2,9 +2,21 @@
 #include <vector>
 #include <cmath>
 
+#include "Matrix.cpp"
+#include "TanhLayer.cpp"
+
 class FullyConnectedLayer {
+private:
+    int noInputNodes;
+    int noOutputNodes;
+    Matrix<float> weights;
+    Matrix<float> biases;
+
+    Matrix<float> weightsDerivatives;
+    Matrix<float> biasesDerivatives;
+    
 public:
-    FullyConnectedLayer (int noNodes): noNodes(noNodes) {
+    FullyConnectedLayer (int _noInputNodes, int _noOutputNodes) {
         vector<double> weights;
         vector<double> biases;
 
