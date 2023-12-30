57b3ebeeca70e0678bc826d0d85a3f383223d527
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2023-12-30 07:44:10 -0500

    Update Network.cpp

--- Network.cpp
+++ Network.cpp
@@ -6,6 +6,15 @@
 
 class Network {
 private:
+    std::vector<FullyConnectedLayer> layers;
+public:
+    explicit FullyConnectedLayer (std::vector<std::vector<int>> dimensions) {
+        // dimensions contain number of input nodes & output nodes in pairs for each of the layers
+        for (int i = 0; i < dimensions.size(); i++) {
+            layers.emplace_back(FullyConnectedLayer(dimensions[i][0], dimensions[i][1]));
+        }
+    }
+
     
 }
 
