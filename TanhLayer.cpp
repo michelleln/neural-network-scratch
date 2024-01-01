cb48587d15fe751dbd5181a97fb26118f7a72cc1
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2024-01-01 11:08:48 -0500

    Update TanhLayer.cpp

--- TanhLayer.cpp
+++ TanhLayer.cpp
@@ -5,11 +5,11 @@
 class TanhLayer{
 public:
     static Matrix<float> forwardPropagate(const Matrix<float> &output) {
-        return Matrix<float>::opElement(output, sigmoid);
+        return Matrix<float>::opElement(output, tanh);
     }
 
     static Matrix<float> getDerivatives(const Matrix<float> &previousLayerOutput, const Matrix<float> &nextLayerDerivatives) {
-        Matrix<float> layerInputDerivatives = Matrix<float>::opElement(previousLayerOutput, [](float n) {return 1 - std::pow(n, 2.0f)); // derivative of tanhx is 1 - tanhx squared
+        Matrix<float> layerInputDerivatives = Matrix<float>::opElement(previousLayerOutput,[](float n) {return 1 - (float)std::pow(n,2.0f);}); // derivative of tanhx is 1 - tanhx squared
         layerInputDerivatives.hProduct(nextLayerDerivatives);
 
         return layerInputDerivatives;
