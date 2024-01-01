bca6367622ba0176b2c4228d5b8ebf616db557d1
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2024-01-01 05:15:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,53 @@
 // main function
 // load training data here
-// initialize a network object
 // output into a csv file
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+#include "Network.cpp"
+
+int main() {
+    // read training entries from csv file
+    std::vector<std::vector<std::string>> content;
+    std::vector<std::string> entry;
+    std::string line;
+    std::string word;
+
+    std::fstream trainingFile("mnist_train.csv", std::ios::in);
+    // get training entry by entry
+    if (trainingFile.is_open()){
+        std::cout << "Reading training data" <<std::endl;
+        while (std::getline(trainingFile, line)){
+            entry.clear();
+            std::stringstream str(line);
+            while(std::getline(str, word, ',')) {
+                entry.push_back(word);
+            }
+            content.push_back(entry);
+        }
+    } else {
+            std::cout << "Could not open file" << std::endl;
+            return 0;
+        }
+    std::cout << "File reading completed" << std::endl;
+
+    std::cout << "Processing training data" << std::endl;
+
+    std::vector<std::vector<Matrix<float>>> trainingData; // transform into format compatible with train fucntion in Network layer
+
+    //skip fieldname line in csv
+    for (int i = 1; i < content.size(); i++) {
+        entry = content[i];
+
+        std::vector<float> inputPixel(784);
+        std::vector<float> outputLabel(10);
+        outputLabel[std::stoi(row[0])] = 1; // only correct label set to 1, rest set to 0
+        for (int j = 1; j < 784 + 1; j++) {
+            inputPixel[j - 1] = std::stof(row[j])/255 // normalize pixel input to have value [0, 1]
+        }
+        trainingData.push_back({Matrix<float>({784, 1}, inputPixel), Matrix<float>({10, 1}, outputLabel)});
+    }
+}
+
