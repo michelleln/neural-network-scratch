59b0c4acdc1cea2e3be3a220e5eb1f9dafb2ecaf
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2024-01-02 03:54:34 -0500

    Create CTestTestfile.cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/hongnhung/Documents/GitHub/scratch-cnn
+# Build directory: /Users/hongnhung/Documents/GitHub/scratch-cnn/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
